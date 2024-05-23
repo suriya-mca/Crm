2bfebeb1061b9b0bee9df6bf7504a0dd994c03c8
Suriya <suriyag1999@gmail.com>
2024-05-23 20:29:18 +0530

    minor change

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('cms-admin-s_/', admin.site.urls),
     path('auth/', include('account.urls')),
     path('cms/', include('cms.urls')),
 ]
